d19539006acd3b31dda1029bb8567f170caa0305
Elias Reid <47339667+eliasreid@users.noreply.github.com>
2021-05-06 23:07:38 -0300

    implement ws oncloses

--- src/app.ts
+++ src/app.ts
@@ -17,20 +17,17 @@ app.get('/', function(req, res, next){
 
 //https://github.com/websockets/ws#how-to-detect-and-close-broken-connections
 // - pongs are automatically sent in response.
-
 // - Could also implement a ping listener on the client side, to notify if lost connection
-
-// Need some way to map clients together. wss.clients gives a Set of clients
-// What happen if I keep a reference to a client outside of the set, then it is removed.
+//So close should work on clean close, if not clean, can implement hearbeats wi
+//https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20780#issuecomment-518051061
 
 let emuId = 0;
 
 //initialize a simple http server
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
 //initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
-
+const wss = new WebSocket.Server({ server: httpServer });
 
 //Probably want to keep track of a list of two-way connections (emu-sessions)
 class EmuSession {
@@ -54,6 +51,8 @@ class EmuSession {
   };
 }
 
+//TOOD: introduced a bug, somehow "emulator" test socket is sending message to itself?
+
 let sessions = new Array<EmuSession>();
 
 //handle incoming connections from clients
@@ -75,16 +74,14 @@ wss.on('connection', (ws: WebSocket, req) => {
       console.log(`connection from type: ${requestType}, id: ${requestId}`);
 
       if(requestType === "emulator"){
+        //TODO: don't assume this!
         //For now, assume unique ID
         sessions.push(new EmuSession(requestId, ws));
+        const session = sessions[sessions.length - 1];
+        console.log(`emulator connection received, creating session. ${sessions.length} total sessions`);
         ws.send(`hello emu with id ${ requestId }`);
         ws.on('message', (message: string) => {
-          //get browser from session id
-          //TODO: can I avoid find here by just using the "pushed" session in upper scope?
-          const session = sessions.find( el => el.id == requestId);
-          //TODO: confirm the ? does what I think it does
-          // Want to check if have a matching sesstion, and if its active
-          if(session?.active()){
+          if(session.active()){
             //There is an active session between the emu and browser, send data to browser
             console.log("emu message received, fwding to browser");
             session.browserClient!.send(message);
@@ -92,6 +89,19 @@ wss.on('connection', (ws: WebSocket, req) => {
             console.log("emu message received, but no active session");
           }
         });
+        //emulator connection close
+        ws.onclose = (event: WebSocket.CloseEvent)=>{
+          //close down session
+          if(session.browserClient != null){
+            session.browserClient.send("emuator client closing");
+            session.browserClient.close();
+          }
+          const num_before = sessions.length;
+          sessions = sessions.filter(function( sess ) {
+            return session !== sess;
+          });
+          console.log(`emulator client closed, sessions from ${num_before} to ${sessions.length}`);
+        }
       }else if(requestType === "browser"){
         ws.send(`hello browser with id ${ requestId }`);
         
@@ -100,6 +110,8 @@ wss.on('connection', (ws: WebSocket, req) => {
         if(session?.emuActive()){
           //add browser to session
           session.browserClient = ws;
+          session.emuClient.send("browser client connected");
+
           ws.on('message', (message: string) => {
             if(session.active()){
               console.log("browser message received, fwding to emu");
@@ -108,6 +120,12 @@ wss.on('connection', (ws: WebSocket, req) => {
               console.log("browser message received, but session not active??");
             }
           });
+
+          //TODO: anything on browser close? we do need to notify emulator
+          ws.onclose = (event: WebSocket.CloseEvent) => {
+            session.emuClient.send("browser client closing");
+          };
+
         }
       }
 
@@ -123,17 +141,54 @@ wss.on('connection', (ws: WebSocket, req) => {
 
 });
 
-wss.on('close', () => {
-  console.log('connection closed. %d clients', wss.clients.size);
-  //TODO: find the session this belongs to - send message to other party, terminate them
-  //TODO: remove session from list
-})
-
+wss.on('error', (event: string, ws: WebSocket) => {
+  console.log(`wss server error, event: ${event}`);
+});
 
 //start our server
-server.listen(process.env.PORT || 8999, () => {
-  let server_addr = server.address();
+httpServer.listen(process.env.PORT || 8999, () => {
+  let server_addr = httpServer.address();
   if(typeof server_addr === 'object' && server_addr != null){
     console.log(`Server started on port ${server_addr.port} :)`);
   }
 });
+
+//server on close is for when server closes, notn clients.
+
+// wss.on('close', (ws: WebSocket) => {
+//   console.log('connection closed. %d clients', wss.clients.size);
+//   //TODO: find the session this belongs to - send message to other party, terminate them
+//   //TODO: remove session from list
+//   var otherWs: WebSocket | undefined;
+//   var emuSession: EmuSession | undefined;
+//   //Could probably do this more efficienlty by mapping with the id?
+//   sessions.forEach( (session) => {
+//     if(session.browserClient == ws){
+//       otherWs = session.emuClient;
+//       emuSession = session;
+//       return;
+//     }
+//     if(session.emuClient == ws){
+//       otherWs = session.browserClient;
+//       emuSession = session;
+//       return;
+//     }
+//   });
+  
+//   //Clean up session if closing socket belonged to one
+//   if(emuSession != null){
+//     if(otherWs != null){
+//       //TODO: send more structured message
+//       otherWs.send("Other socket disconnect, terminating");
+//       otherWs.terminate();
+//     }
+//     //remove session from list
+//     console.log(`before filter, ${sessions.length} total sessions`);
+//     sessions = sessions.filter(function( session ) {
+//       return session !== emuSession;
+//     });
+//     console.log(`after filter, ${sessions.length} total sessions`);
+//   }
+// });
+
+
